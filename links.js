8b578bc6cf696ff951d44fbc54e70ec6ed4fe13a
Simon Willans <stwillans@gmail.com>
2016-09-27 15:22:54 +0100

    Create links.js

---
+++ links.js
@@ -0,0 +1,23 @@
+$.extend($.expr[':'], {
+	external: function(obj) {
+
+		return (obj.hostname !== window.location.hostname) && /:\/\//.test($(obj).attr('href'));
+
+	},
+	internal: function(obj) {
+
+		return (obj.hostname === window.location.hostname) || !/:\/\//.test($(obj).attr('href'));
+
+	},
+});
+
+$(document.body).on('click', 'a:external:not(.internal), a.external, a[rel="external"], a[href$=".pdf"]', function(e) {
+
+	if (e.which !== 2) {
+
+		window.open($(this).attr('href'));
+		e.preventDefault();
+
+	}
+
+});
